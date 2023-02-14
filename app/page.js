59588a3faabab654f51f707411fb8273b4b19a0a
unknown <yousefriad9@gmail.com>
2023-02-15 02:55:33 +0400

    performance improvement

--- app/page.js
+++ app/page.js
@@ -10,6 +10,7 @@ export default async function Home() {
     `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
   );
   const res = await data.json();
+  
   return (
     <main>
       <h1>MovieLand</h1>
